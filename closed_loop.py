bf2095159ece3d5d8e7bb71789b84953f75121c5
Aavash1738 <bhattaraiaavash1@gmail.com>
2024-08-06 22:37:42 +0545

    added closed loop solution to the problem

--- closed_loop.py
+++ closed_loop.py
@@ -1,5 +1,5 @@
-from random import uniform, randint, sample 
-import time 
+import random
+import time
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -10,7 +10,7 @@ def visualize_distance_matrix(distance_matrix):
     for i in range(num_nodes):
         G.add_node(i)
     for i in range(num_nodes):
-        for j in range(i+1, num_nodes):
+        for j in range(i + 1, num_nodes):
             weight = distance_matrix[i][j]
             G.add_edge(i, j, weight=weight)
     pos = nx.spring_layout(G)
@@ -21,68 +21,38 @@ def visualize_distance_matrix(distance_matrix):
     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, alpha=0.4)
     plt.show()
 
-a = time.time()
-
-# distanceMatrix = [
-# [0, 29, 20, 21, 16],  # data-set(TSP)
-# [29, 0, 15, 29, 28],
-# [20, 15, 0, 15, 14],
-# [21, 29, 15, 0, 4],
-# [16, 28, 14, 4, 0]]
-
-# distanceMatrix = [
-# [0, 29, 20, 21, 16],  # data-set(TSP)
-# [29, 0, 15, 29, 28],
-# [20, 15, 0, 15, 14],
-# [21, 29, 15, 0, 4],
-# [16, 28, 14, 4, 0]]
-
-# distanceMatrix = [    
-#     [0, 10, 15, 20, 25], # data-set(TSP)
-#     [10, 0, 35, 25, 30],
-#     [15, 35, 0, 30, 15],
-#     [20, 25, 30, 0, 20],
-#     [25, 30, 15, 20, 0]]
-
-# distanceMatrix = [
-#     [0, 21, 29, 15, 22], # data-set(TSP)
-#     [21, 0, 17, 28, 32],
-#     [29, 17, 0, 25, 24], #4 bata start bhairachha
-#     [15, 28, 25, 0, 30],
-#     [22, 32, 24, 30, 0]]
-
+# Distance matrix (TSP dataset)
 distanceMatrix = [
-    [0, 18, 23, 31, 27], # data-set(TSP)
+    [0, 18, 23, 31, 27],
     [18, 0, 34, 26, 20],
-    [23, 34, 0, 19, 30], #3 bata start bhairachha 
+    [23, 34, 0, 19, 30],
     [31, 26, 19, 0, 22],
-    [27, 20, 30, 22, 0]]
+    [27, 20, 30, 22, 0]
+]
 
 visualize_distance_matrix(distanceMatrix)
 
 def calculateCost(solution):
-    totalDistance = 0 
+    totalDistance = 0
     index = solution[0]
     for nextIndex in solution[1:]:
         totalDistance += distanceMatrix[index][nextIndex]
         index = nextIndex
-    totalDistance += distanceMatrix[index][solution[0]]  # Include return to starting point
-    return totalDistance 
+    totalDistance += distanceMatrix[index][solution[0]]  # Return to the start
+    return totalDistance
 
-def swap(sequence, i, j):  
+def swap(sequence, i, j):
     sequence[i], sequence[j] = sequence[j], sequence[i]
 
 def randF():
-    return uniform(0.0001, 0.9999)
-    
-goMethods = int(2 + ((randF() - 0.5) * 2) * (2.5 - 1.2))
+    return random.uniform(0.0001, 0.9999)
 
-def roulettaSelection(bees):
+def rouletteSelection(bees):
     total = sum(1 / float(bees[i][1]) for i in range(len(bees)))
     probability = []
     section = 0
     for i in range(len(bees)):
-        section += float((1 / int(bees[i][1])) / total)
+        section += float((1 / bees[i][1]) / total)
         probability.append(section)
     nextGeneration = []
     for i in range(numFeeds):
@@ -91,14 +61,23 @@ def roulettaSelection(bees):
             if choice <= probability[j]:
                 nextGeneration.append(bees[j])
                 break
-    return sample(nextGeneration, numFeeds)
+    return random.sample(nextGeneration, numFeeds)
 
-def approachtoGood(bees, a, b, c):
+def approachToGood(bees, a, b, c):
     bees = bees[0][:]
     swap(bees, a, b)
     swap(bees, b, c)
     return (bees, calculateCost(bees))
 
+def removeBees(bees):
+    bees = bees[0][:]
+    indices = random.sample(range(n), 4)
+    swap(bees, indices[0], indices[1])
+    swap(bees, indices[1], indices[2])
+    swap(bees, indices[2], indices[3])
+    return (bees, calculateCost(bees))
+
+# Parameters
 numBees = 10
 workerBees = 5
 iteration = 100
@@ -106,33 +85,29 @@ numFeeds = 5
 limitsOfTry = 5
 n = len(distanceMatrix)
 bees = []
-firstPath = list(range(0, n))
+firstPath = list(range(1, n))  # Start from node 1 to n-1 (excluding 0)
 
+# Initialize bees
 for i in range(numBees):
-    path = sample(firstPath, n)
+    path = [0] + random.sample(firstPath, n - 1) + [0]  # Start and end at node 0
     bees.append((path, calculateCost(path)))
 bees.sort(key=lambda x: x[1])
 
-def removeBees(bees):
-    bees = bees[0][:]
-    indices = sample(range(n), 4)
-    swap(bees, indices[0], indices[1])
-    swap(bees, indices[1], indices[2])
-    swap(bees, indices[2], indices[3])
-    return (bees, calculateCost(bees))
-        
+a = time.time()
+
+# Main algorithm loop
 while iteration != 0:
     count = 0
-    bestBees = bees[randint(0, goMethods)]
+    bestBees = bees[random.randint(0, int(2 + ((randF() - 0.5) * 2) * (2.5 - 1.2)))]
     for j in range(0, n):
-        morePowerBees = approachtoGood(bestBees, randint(0, n-1), randint(0, n-1), randint(0, n-1))
+        morePowerBees = approachToGood(bestBees, random.randint(1, n-1), random.randint(1, n-1), random.randint(1, n-1))
         if bees[j][1] > morePowerBees[1]:
             bees[j] = morePowerBees
         else:
             limitsOfTry += 1
     bees.sort(key=lambda x: x[1])
-    for i in range(numBees-workerBees, numBees):
-        observer = roulettaSelection(bees)
+    for i in range(numBees - workerBees, numBees):
+        observer = rouletteSelection(bees)
         for l in range(workerBees, numBees):
             bees[l] = observer[count]
     count += 1
@@ -141,10 +116,11 @@ while iteration != 0:
             bees[k] = removeBees(bees[k])
     bees.sort(key=lambda x: x[1])
     iteration -= 1
+
 b = time.time()
 
 # Include return to starting point in the output
-best_path = bees[0][0] + [bees[0][0][0]]
+best_path = bees[0][0]
 print("Best path: ", best_path)
 print("Best cost: ", bees[0][1])
-print("Algorithm runtime: ", b-a)
+print("Algorithm runtime: ", b - a)
